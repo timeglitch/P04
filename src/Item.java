061cad1fb7787b7dec0dd79f8460051c0a1f76a1
TimeGlitch <frankj.zhang@gmail.com>
2022-10-10 15:11:33 -0500

    complete item code

--- src/Item.java
+++ src/Item.java
@@ -2,7 +2,13 @@ public class Item {
     String description;
     int expirationDate;
 
-    public Item(String description, int expirationDate) {
+    public Item(String description, int expirationDate) throws IllegalArgumentException {
+        if(expirationDate < 0) {
+            throw new IllegalArgumentException("expiration date is invalid");
+        }
+        if(description == null || description == "") {
+            throw new IllegalArgumentException("description is invalid");
+        }
         this.description = description;
         this.expirationDate = expirationDate;
     }
@@ -16,6 +22,9 @@ public class Item {
     }
 
     public void setDescription(String input) {
+        if(description == null || description.equals("")) {
+            throw new IllegalArgumentException("description is invalid");
+        }
         description = input;
     }
 
@@ -23,5 +32,9 @@ public class Item {
     public String toString() {
         return description + ": " + Integer.toString(expirationDate);
     }
-    
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Item && other.getDescription().equals(description);
+    }
 }
